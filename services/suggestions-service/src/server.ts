53823e5722f0a8ee2ed2aacbafe9f32e98772215
Dan-Lucian Rosu <rosudanlucian94@gmail.com>
2021-09-05 16:12:43 +0300

    Use the logging level from the config

--- services/suggestions-service/src/server.ts
+++ services/suggestions-service/src/server.ts
@@ -1,10 +1,11 @@
 import Fastify, { FastifyInstance } from "fastify";
 import { loggingStreams, loggingSerializers } from "./config/logging";
 import { searchRoutes } from "./routes/searchRoute";
+import { loggingLevel } from "./config/environment";
 
 const server: FastifyInstance = Fastify({
   logger: {
-    level: "info",
+    level: loggingLevel,
     stream: loggingStreams,
     serializers: loggingSerializers,
   },
